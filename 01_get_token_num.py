74c783013ce64df0d6a11bca9d0c2083b4f9a595
Jchench <jessiechen2026@u.northwestern.edu>
2024-06-18 16:24:23 -0500

    organization and started getting tokens

--- 01_get_token_num.py
+++ 01_get_token_num.py
@@ -12,7 +12,16 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
     num_tokens = len(encoding.encode(string))
     return num_tokens
   
-PUBL203 = with open("src/txt/machine_readable/PUBL203.txt", "r") as f:
+# PUBL203
+
+with open('clean_law_texts/src/machine_readable/PUBL203.txt', 'r') as f:
   PUBL203_text = f.read()
 
-num_tokens_from_string(, "cl100k_base")
+PUBL203_num = num_tokens_from_string(PUBL203_text, "cl100k_base")
+
+# PUBL174
+
+with open('clean_law_texts/src/machine_readable/PUBL174.txt', 'r') as f:
+  PUBL174_text = f.read()
+
+PUBL174_num = num_tokens_from_string(PUBL174_text, "cl100k_base")
